87f90174b8fb082f1582cae2a8f4213afe192e5a
ffermin <ferminflorio@gmail.com>
2025-03-11 10:20:50 -0600

    js onclick img

--- app.js
+++ app.js
@@ -7,5 +7,32 @@ const observer = new IntersectionObserver((entries) => {
 	});
 });
 
+const observerTranslate = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add("show_2");
+		}
+	});
+});
+
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+const zoomElements = document.querySelectorAll(".hidden_2");
+zoomElements.forEach((el) => observerTranslate.observe(el));
+
+document.addEventListener("DOMContentLoaded", () => {
+	// Seleccionamos todas las imágenes con la clase "clickable-img"
+	const images = document.querySelectorAll(".clickable-img");
+
+	images.forEach((img) => {
+		img.addEventListener("click", () => {
+			// Verificamos el tamaño actual y alternamos entre grande y original
+			if (img.style.height === "650px") {
+				img.style.height = "550px"; // Tamaño original
+			} else {
+				img.style.height = "650px"; // Se agranda
+			}
+		});
+	});
+});
